a5255d8a1ccc21be5ca15fc5b6426667971d8f49
BaoxRosie1 <94978764+BaoxRosie1@users.noreply.github.com>
2023-03-18 12:15:57 +0700

    commitchoTA

--- components/Hero.js
+++ components/Hero.js
@@ -3,20 +3,28 @@ import Navbar from "./Navbar/Navbar";
 
 const Hero = () => {
   return (
-    <div className="container mx-auto my-10 ">
-      <div className="bg-[#fff5f1] px-6 lg:px-16 py-10 rounded-3xl">
-        <div className="flex flex-col lg:flex-row justify-center  lg:justify-between items-center gap-5">
-          <div className="flex flex-col gap-5 lg:gap-10">
-            <p className=" text-2xl sm:text-4xl md:text-5xl lg:text-6xl font-medium">
-              A Unique approach <br className="lg:flex hidden"/> to learning foreign <br className="lg:flex hidden" /> languages
-              online
-            </p>
-            <p className="text-sm sm:text-base lg:text-lg text-gray-500">
-              Learn at your own pace, with lifetime <br  className="lg:flex hidden"/> access on mobile and desktop
-            </p>
-            <button className="btn btn-sm lg:btn-lg bg-[#524fd5] text-white rounded-full border-none w-36 lg:w-44 capitalize">Get Started</button>
+    <div className="bg-[#0b1a4a] w-full px-6 lg:px-16 py-10">
+      <div className="flex flex-col lg:flex-row justify-center  lg:justify-between items-center gap-5">
+        <img src="/assets/sac.png" alt="" className="w-[800px] h-[800px]" />
+        <div className="flex flex-col gap-5 lg:gap-10 pl-[20px]">
+          <p className=" text-[75px] text-white sm:text-4xl md:text-5xl lg:text-[85px] font-bold leading-[1.3rem]">
+            Giải pháp <br className="lg:flex hidden" /> sạc xe điện{" "}
+            <br className="lg:flex hidden" /> tối ưu
+          </p>
+          <p className="text-sm sm:text-base lg:text-lg text-white">
+            Bộ sạc ô tô điện treo tường Aurora Series (7.4kW; 11kW; 22kW) Chuyên
+            nghiệp và tiện lợi – Sẽ giúp bạn tiết kiệm thời gian và tiền bạc.
+          </p>
+          <div className="flex flex-row gap-12 ">
+            <div>
+              <button className="btn btn-sm lg:btn-lg bg-[#00ffff] color-[#0b1a4a] rounded-full border-none w-[50px] lg:w-[250px] capitalize">
+                Nhận báo giá ▶
+              </button>
+            </div>
+            <div>
+              <button className="text-white pt-[20px]">Tư vấn kỹ thuật ▶</button>
+            </div>
           </div>
-          <img src="/assets/hero.png" alt="" />
         </div>
       </div>
     </div>
