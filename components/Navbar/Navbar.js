65b5b125b4d64d6a97ea486085da7286174f358a
QuocDat1219 <lenguyenquocdat1219@gmail.com>
2023-03-18 12:37:38 +0700

    create navbar sticky

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className="bg-[#0B1A4A] h-[80px] w-full lg:flex hidden md:block md:text-center justify-around items-center pl-20 pr-20">
+    <div className="bg-[#0B1A4A] h-[80px] w-full lg:flex hidden md:block md:text-center justify-around items-center fixed pl-20 pr-20 z-10 top-0 border-b border-gray-300">
       <div className="w-[100%] lg:text-left md:text-center">
         <span className=" text-[#FFF] text-[15px] font-Muli text-left hover:text-[#00FFFF]">
           Hotline: 0931 848 775
@@ -12,7 +12,7 @@ const Navbar = () => {
           phukienotodien68@gmail.com
         </span>
       </div>
-      <div className="w-[100%] font-Muli lg:justify-end md:justify-center flex text-[#9FA6C0]">
+      <div className="w-[100%] font-Muli lg:justify-end md:justify-center flex text-[#9FA6C0] md:pt-5 lg:pt-0">
         <div class="flex-initial w-32 hover:text-[#FFFF]" >
           <a className="text-[14px]" href="https://www.facebook.com/groups/532055499048261">FACEBOOK</a>
         </div>
